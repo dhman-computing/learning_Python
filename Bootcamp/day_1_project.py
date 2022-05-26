59914813b43b0b9be8f4b41e9dd0827415038187
Dhiman Chakrabarty <78009274+dhman-computing@users.noreply.github.com>
2022-05-26 06:34:43 +0000

    Completed Day 1 Project

---
+++ Bootcamp/day_1_project.py
@@ -0,0 +1,6 @@
+## Band Name Generator
+# This program is supose to generate the name of a bnd by combining the name of the city in which the person grew up and the name of the person's pet
+
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be", city, pet)
